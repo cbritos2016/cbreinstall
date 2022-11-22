2b8d4b1e15bc9f3b4a9f1103eb92c6af552e041d
cbritos2016 <cristian@britos.net>
2022-11-22 13:16:43 -0500

    Update runSoft.sh

--- runSoft.sh
+++ runSoft.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 
-sudo dnf install terminator
-sudo dnf install remmina
-sudo dnf install xinput
+sudo dnf install terminator -y
+sudo dnf install remmina -y
+sudo dnf install xinput -y
 
 sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
 sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-sudo dnf install sublime-text
+sudo dnf install sublime-text -y
 
 sudo dnf install flameshot -y
 sudo dnf install gedit -y
-sudo dnf install dnf-plugins-core
+sudo dnf install dnf-plugins-core -y 
 
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo dnf install brave-browser
+sudo dnf install brave-browser -y
 
-sudo dnf group install Virtualization
-sudo dnf group install "C Development Tools and Libraries" "Development Tools"
-sudo dnf install cmake
+sudo dnf group install Virtualization -y
+sudo dnf group install "C Development Tools and Libraries" "Development Tools" -y
+sudo dnf install cmake -y
 
 curl -L https://nixos.org/nix/install | sh
