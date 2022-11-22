377621b3cc980a88573b126ee6c9f256abbe701c
cbritos2016 <cristian@britos.net>
2022-11-22 13:49:21 -0500

    Update runSoft.sh

--- runSoft.sh
+++ runSoft.sh
@@ -21,5 +21,12 @@ sudo dnf group install "C Development Tools and Libraries" "Development Tools" -
 sudo dnf install cmake -y
 sudo dnf install drawing -y
 
+sudo su
+echo '3.134.185.143 openvpnserver' >> /etc/hosts
+echo '65.21.238.137 hetzner' >> /etc/hosts
+
+sudo usermod -a -G qemu crisb
+sudo usermod -a -G kvm crisb
+sudo usermod -a -G libvirt crisb
 
 curl -L https://nixos.org/nix/install | sh
